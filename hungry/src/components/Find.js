1f65522f6e60c64959fe6df1fe4b5c88f05fd56f
Ashwin Talwalkar <atalwalkar719@gmail.com>
2021-07-08 21:04:54 -0700

    Bug on find

--- hungry/src/components/Find.js
+++ hungry/src/components/Find.js
@@ -4,13 +4,17 @@ const Find = () => {
     const [showMeal, setShowMeal] = useState(false);
     const [id, setId] = useState(null);
 
-    const handleSubmit= (e) => {
-        console.log(id);
-        if(id != null){
-            setShowMeal(true); 
-        }
-        console.log(showMeal);
-    
+    const form = useRef();
+
+    const handleSubmit= () => {
+            setId(form.target.value);
+            console.log(id);
+            console.log(showMeal);
+            if(id != null){
+                setShowMeal(true); 
+            }
+           
+        
     }
 
     const MealShowCase = (props) => {
@@ -25,14 +29,14 @@ const Find = () => {
                 </div>
 
                 <div className="flexBoxContainer " style = {{justifyContent: 'center', marginTop: '10vh'}}>
-                <form onSubmit={handleSubmit}>
+                <form>
                     <label >
                         <h2 style = {{color: '#FF6D00'}}>Cuisine Type</h2>
                     </label>
 
-                    <input className = 'formz' key="forms" value = {id} onChange = {(e)=> setId(e.target.value)} />
+                    <input className = 'formz' type = 'text' ref= {form} />
 
-                    <button className="fav" style = {{  marginLeft: '0.75vw'}}><span>Submit</span></button>
+                    <input type = 'submit' onClick = {()=>handleSubmit}className="fav" style = {{  marginLeft: '0.75vw'}} value = 'Submit'></input>
                         
                     
                     </form>         
